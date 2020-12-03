171a0a1d6d8a8af830c87be17fe8212fc0b3fac0
FaustVX <jodu035@gmail.com>
2020-12-03 16:25:56 +0100

    Y2020D03P2

--- 2020/Day03/Solution.cs
+++ 2020/Day03/Solution.cs
@@ -13,15 +13,16 @@ namespace AdventOfCode.Y2020.Day03
 
         public IEnumerable<object> Solve(string input)
         {
-            yield return PartOne(3, input.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(line => line.Select(c => c is '.').ToArray()).ToArray());
-            yield return PartTwo(input);
+            var terrain = input.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(line => line.Select(c => c is '.').ToArray()).ToArray();
+            yield return CalcutateTrees(1, 3, terrain);
+            yield return new[]{ (1, 1), (1, 3), (1, 5), (1, 7), (2, 1) }.Select(t => CalcutateTrees(t.Item1, t.Item2, terrain)).Aggregate(1L, (a, c) => a * c);
         }
 
-        int PartOne(int right, bool[][] terrain)
+        int CalcutateTrees(int down, int right, bool[][] terrain)
         {
             var x = 0;
             var trees = 0;
-            for (int y = 0; y < terrain.Length; y++)
+            for (int y = 0; y < terrain.Length; y += down)
             {
                 if(!terrain[y][x % terrain[y].Length])
                     trees++;
@@ -29,7 +30,5 @@ namespace AdventOfCode.Y2020.Day03
             }
             return trees;
         }
-
-        int PartTwo(string input) => 0;
     }
 }
