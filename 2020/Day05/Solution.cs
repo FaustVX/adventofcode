e7292658a459443d85b5226bce04cee12739cc83
FaustVX <jodu035@gmail.com>
2020-12-05 22:47:58 +0100

    Using Binary form

--- 2020/Day05/Solution.cs
+++ 2020/Day05/Solution.cs
@@ -14,38 +14,11 @@ namespace AdventOfCode.Y2020.Day05
         public IEnumerable<object> Solve(string input)
         {
             yield return input.SplitLine().Select(CalculateSeatId).Max();
-            yield return input.SplitLine().Select(CalculateSeatId).OrderBy(id => id).SelectWithLast().TakeWhile(t => t.current - t.last is 1).Last().current + 1;
+            var hashSets = input.SplitLine().Select(CalculateSeatId).ToList();
+            yield return Enumerable.Range(hashSets.Min(), hashSets.Max() - hashSets.Min()).First(bp => !hashSets.Contains(bp));
         }
 
         int CalculateSeatId(string boardingPass)
-        {
-            var (minRow, maxRow, lenRow) = (0, 127, 128);
-            var (minCol, maxCol, lenCol) = (0, 7, 8);
-
-            foreach (var c in boardingPass)
-            {
-                switch (c)
-                {
-                    case 'F':
-                        lenRow /= 2;
-                        maxRow = minRow + (lenRow - 1);
-                        break;
-                    case 'B':
-                        lenRow /= 2;
-                        minRow = maxRow - (lenRow - 1);
-                        break;
-                    case 'L':
-                        lenCol /= 2;
-                        maxCol = minCol + (lenCol - 1);
-                        break;
-                    case 'R':
-                        lenCol /= 2;
-                        minCol = maxCol - (lenCol - 1);
-                        break;
-                }
-            }
-
-            return minRow * 8 + minCol;
-        }
+            => Convert.ToInt32(string.Concat(boardingPass.Select(l => l is 'B' or 'R' ? '1' : '0')), 2);
     }
 }
