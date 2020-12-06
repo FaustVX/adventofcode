732244d4ee74c09e0d3f604b2b3a66b6a84f1381
FaustVX <jodu035@gmail.com>
2020-12-06 18:20:34 +0100

    Y2020D06P2

--- 2020/Day06/Solution.cs
+++ 2020/Day06/Solution.cs
@@ -20,6 +20,23 @@ namespace AdventOfCode.Y2020.Day06
         int PartOne(string input)
             => Regex.Split(input, "\r?\n\r?\n").Select(group => group.SplitLine().SelectMany(l => l).ToHashSet().Count).Sum();
 
-        int PartTwo(string input) => 0;
+        int PartTwo(string input)
+        {
+            var sum = 0;
+            foreach (var group in Regex.Split(input, "\r?\n\r?\n"))
+            {
+                var dictionary = Enumerable.Range(0, 26).ToDictionary(i => (char)('a' + i), _ => 0);
+                var people = group.SplitLine();
+                foreach (var person in people)
+                {
+                    foreach (var answer in person)
+                    {
+                        dictionary[answer]++;
+                    }
+                }
+                sum += dictionary.Values.Count(v => v == people.Length);
+            }
+            return sum;
+        }
     }
 }
