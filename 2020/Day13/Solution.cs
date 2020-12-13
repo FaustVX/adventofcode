007c06387f311ed8fa59291ec4592fe3ee51ad01
FaustVX <jodu035@gmail.com>
2020-12-13 19:00:30 +0100

    Simplified P1

--- 2020/Day13/Solution.cs
+++ 2020/Day13/Solution.cs
@@ -19,17 +19,11 @@ namespace AdventOfCode.Y2020.Day13
 
         int PartOne(string input)
         {
-            var splitted = input.SplitLine();
-            var timeStamp = int.Parse(splitted[0]);
-            var lists = splitted[1].Extract<List<string>>(@"(?:(\d+|x),?)+")!
-                .Select(s => (ok: int.TryParse(s, out var bus), bus))
-                .Where(t => t.ok)
-                .Select(t => t.bus)
-                .Select(bus => (bus, mod: -(timeStamp % bus) + bus))
+            var (timeStamp, buses) = input.Extract<(int, List<int>)>(@"(\d+)\W+(?:(?:(\d+)|x),?)+")!;
+            return buses.Select(bus => (bus, mod: -(timeStamp % bus) + bus))
                 .OrderBy(t => t.mod)
                 .Select(t => t.bus * t.mod)
                 .First();
-            return lists;
         }
 
         int PartTwo(string input)
