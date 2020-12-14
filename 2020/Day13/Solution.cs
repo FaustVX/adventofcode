7a351a25315c16dc5341a2cca3f52487dbf6c69e
FaustVX <jodu035@gmail.com>
2020-12-14 04:27:09 +0100

    Y2020D13P2 (Brute force)

--- 2020/Day13/Solution.cs
+++ 2020/Day13/Solution.cs
@@ -26,9 +26,39 @@ namespace AdventOfCode.Y2020.Day13
                 .First();
         }
 
-        int PartTwo(string input)
+        ulong PartTwo(string input)
         {
-            return 0;
+            var (_, buses) = input.Extract<(int, List<string> buses)>(@"(\d+)\W+(?:(\d+|x),?)+")!;
+            var list = buses.Select((bus, pos) => (bus, pos))
+                .Select(t => ((int.TryParse(t.bus, out var bus), bus), t.pos))
+                .Where(t => t.Item1.Item1)
+                .Select(t => (bus: (ulong)t.Item1.bus, pos: (ulong)t.pos))
+                .ToArray();
+
+            var max = list.OrderByDescending(l => l.bus).First();
+            var i = list.Length > 5 ? 100_000_000_000_000u : 1068_000;
+            i = i + max.bus - (i % max.bus) - max.pos;
+            // var i = max.bus - max.pos;
+            while (Cond(i))
+                i = checked(i + max.bus);
+
+            return i;
+
+            bool Cond(ulong ts)
+                => !list.All(l => ((ts + l.pos) % l.bus) is 0);
+        }
+    }
+
+    public static class Ext
+    {
+        public static IEnumerable<(T last, T curr)> Slide<T>(this IEnumerable<T> source)
+        {
+            var enumerable = source.GetEnumerator();
+            if (!enumerable.MoveNext())
+                yield break;
+            var last = enumerable.Current;
+            while (enumerable.MoveNext())
+                yield return (last, last = enumerable.Current);
         }
     }
 }
