3cd0b183afac5a97bec85930f5282491063067e1
FaustVX <jodu035@gmail.com>
2021-12-04 11:33:46 +0100

    Y2021D04P1

---
+++ 2021/Day04/Solution.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode.Y2021.Day04;
+
+[ProblemName("Giant Squid")]
+class Solution : Solver
+{
+    class Board
+    {
+        public (byte value, bool marked)[,] Grid { get; } = new (byte, bool)[5, 5];
+
+        public Board(string[] input)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                var splitted = input[y].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int x = 0; x < 5; x++)
+                    Grid[x, y] = (byte.Parse(splitted[x]), false);
+            }
+        }
+
+        public bool IsBingo()
+        {
+            if (Enumerable.Range(0, 5).Any(CheckColumn))
+                return true;
+            if (Enumerable.Range(0, 5).Any(CheckLine))
+                return true;
+
+            return false;
+
+            bool CheckLine(int y)
+            {
+                for (int x = 0; x < 5; x++)
+                    if (!Grid[x, y].marked)
+                        return false;
+                return true;
+            }
+
+            bool CheckColumn(int x)
+            {
+                for (int y = 0; y < 5; y++)
+                    if (!Grid[x, y].marked)
+                        return false;
+                return true;
+            }
+        }
+
+        public void Mark(byte number)
+        {
+            for (int x = 0; x < 5; x++)
+                for (int y = 0; y < 5; y++)
+                {
+                    ref var value = ref Grid[x, y];
+                    if (value.value == number)
+                        value.marked = true;
+                }
+        }
+
+        public static Board Parse(string input)
+            => new(input.SplitLine());
+
+        internal IEnumerable<byte> GetUnmarkedValues()
+            => Grid.Cast<(byte number, bool marked)>()
+                .Where(static cell => !cell.marked)
+                .Select(static cell => cell.number);
+
+    }
+
+    private static (byte[] numbers, Board[] boards) Parse(string input)
+    {
+        var datas = input.Split2Lines();
+        var numbers = datas[0].Split(',')
+            .Select(static n => byte.Parse(n))
+            .ToArray();
+
+        var boards = datas[1..]
+            .Select(Board.Parse)
+            .ToArray();
+
+        return (numbers, boards);
+    }
+
+    public object PartOne(string input)
+    {
+        var (numbers, boards) = Parse(input);
+
+        foreach (var number in numbers)
+            foreach (var board in boards)
+            {
+                board.Mark(number);
+                if (board.IsBingo())
+                    return number * board.GetUnmarkedValues().Sum(static v => (int)v);
+            }
+        throw new();
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
