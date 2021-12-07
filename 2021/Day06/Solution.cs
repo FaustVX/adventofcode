7d7223819aa10f663551840bab8f99062caa1939
FaustVX <jodu035@gmail.com>
2021-12-07 18:56:10 +0100

    Y2021D06 cleanup

--- 2021/Day06/Solution.cs
+++ 2021/Day06/Solution.cs
@@ -23,30 +23,21 @@ class Solution : Solver
 
     private static ulong Solve(string input, int days)
     {
-        // fishes: TKey => Timer; TValue => Number of fishes
-        var fishes = Parse(input)
-            .GroupBy(static i => i)
-            .ToDictionary(static group => group.Key, static group => group.LongCount());
+        var fishes = new ulong[9];
 
-        Sanitize(fishes, 8, 0);
+        foreach (var fish in Parse(input))
+            fishes[fish]++;
 
         for (var day = 0; day < days; day++)
-        {
-            fishes = Enumerable.Range(0, 9).ToDictionary(static i => i, i => i switch
-            {
-                6 => fishes[0] + fishes[7],
-                8 => fishes[0],
-                _ => fishes[i + 1],
-            });
-        }
+            fishes = Enumerable.Range(0, 9)
+                .Select(i => i switch
+                    {
+                        6 => fishes[0] + fishes[7],
+                        8 => fishes[0],
+                        _ => fishes[i + 1],
+                    })
+                .ToArray();
 
-        return fishes.Values.Aggregate(0UL, static (acc, i) => acc + (ulong)i);
-
-        static void Sanitize<TValue>(Dictionary<int, TValue> dictionary, int maxValue, TValue defaultValue)
-        {
-            for (int i = 0; i <= maxValue; i++)
-                if (!dictionary.ContainsKey(i))
-                    dictionary[i] = defaultValue;
-        }
+        return fishes.Aggregate(0UL, static (acc, i) => acc + i);
     }
 }
