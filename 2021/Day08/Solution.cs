415af74336e495253d82870cf035ceb37e06cb30
FaustVX <jodu035@gmail.com>
2021-12-09 01:47:58 +0100

    Y2021D08P2

--- 2021/Day08/Solution.cs
+++ 2021/Day08/Solution.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Text;
 using System.Runtime.InteropServices;
+using System.Collections.ObjectModel;
+using AdventOfCode.Y2020.Day19;
 
 namespace AdventOfCode.Y2021.Day08;
 
@@ -13,17 +15,75 @@ class Solution : Solver
 {
     private class Entry
     {
-        public char[][] Patterns { get; init; }
-        public char[][] Values { get; init; }
+        public Digit[] Patterns { get; init; }
+        public Digit[] Values { get; init; }
+        public Dictionary<char, char> Links { get; }
+        private Entry(Digit[] patterns, Digit[] values)
+        {
+            (Patterns, Values) = (patterns, values);
+            var remainingDigits = Patterns.ToList();
+            var _1 = remainingDigits.Single(static digit => digit.Length is 2);
+            remainingDigits.Remove(_1);
+            var _cOr_f = _1.Segments.ToList();
+            var _7 = remainingDigits.Single(static digit => digit.Length is 3);
+            remainingDigits.Remove(_7);
+            var _a = _7.Segments.Except(_1.Segments).Single();
+            var _4 = remainingDigits.Single(static digit => digit.Length is 4);
+            remainingDigits.Remove(_4);
+            var _bOr_d = _4.Segments.Except(_1.Segments).ToList();
+            var _abcdf = _bOr_d.Concat(_cOr_f).Append(_a).ToList();
+            var _9 = remainingDigits.Where(static digit => digit.Length is 6)
+                .Single(digit => digit.Segments.Except(_abcdf).Count() is 1);
+            remainingDigits.Remove(_9);
+            var _g = _9.Segments.Except(_abcdf).Single();
+            var _8 = remainingDigits.Single(static digit => digit.Length is 7);
+            remainingDigits.Remove(_8);
+            var _e = _8.Segments.Except(_9.Segments).Single();
+            var _0Or_6 = remainingDigits.Where(static digit => digit.Length is 6).ToList();
+            var _cOr_d = _0Or_6[0].Segments.Except(_0Or_6[1].Segments).ToList();
+                _cOr_d.Add(_0Or_6[1].Segments.Except(_0Or_6[0].Segments).Single());
+            var _c = _cOr_d.Intersect(_cOr_f).Single();
+            var _d = _cOr_d.Except(Enumerable.Repeat(_c, 1)).Single();
+            var _f = _cOr_f.Except(Enumerable.Repeat(_c, 1)).Single();
+            var _b = _bOr_d.Except(Enumerable.Repeat(_d, 1)).Single();
+
+            Links = new(capacity: 7)
+            {
+                [_a] = 'a',
+                [_b] = 'b',
+                [_c] = 'c',
+                [_d] = 'd',
+                [_e] = 'e',
+                [_f] = 'f',
+                [_g] = 'g',
+            };
+        }
         public static Entry Parse(string input)
         {
             var (first, (end, _)) = input.Split(" | ");
-            return new()
-            {
-                Patterns = first.Split(' ').ParseToArrayOfT(Enumerable.ToArray),
-                Values = end.Split(' ').ParseToArrayOfT(Enumerable.ToArray),
-            };
+            return new(
+                first.Split(' ').ParseToArrayOfT(Digit.Parse),
+                end.Split(' ').ParseToArrayOfT(Digit.Parse)
+            );
         }
+    }
+
+    public class Digit
+    {
+        public char[] Segments { get; init; }
+        public int Length => Segments.Length;
+
+        public static Digit Parse(string input)
+            => new() { Segments = input.ToCharArray() };
+
+        public Digit Convert(IReadOnlyDictionary<char, char> links)
+            => new()
+            {
+                Segments = Segments.Select(seg => links[seg]).ToArray()
+            };
+
+        public void Deconstruct(out bool a, out bool b, out bool c, out bool d, out bool e, out bool f, out bool g)
+            => (a, b, c, d, e, f, g) = (Segments.Contains('a'), Segments.Contains('b'), Segments.Contains('c'), Segments.Contains('d'), Segments.Contains('e'), Segments.Contains('f'), Segments.Contains('g'));
     }
 
     private static IEnumerable<Entry> Parse(string input)
@@ -34,6 +94,32 @@ class Solution : Solver
 
     public object PartTwo(string input)
     {
-        return 0;
+        var entries = Parse(input).Select(static entry => entry.Values.Select(digit => digit.Convert(entry.Links)).ToArray());
+        var sum = 0L;
+        foreach (var entry in entries)
+        {
+            var value = 0;
+            foreach (var digit in entry)
+            {
+                value *= 10;
+#pragma warning disable CS8509 // The switch expression does not handle some values of its input type (it is not exhaustive) involving an unnamed enum value. For example, the pattern ‘...’ is not covered.
+                value += digit switch
+#pragma warning restore
+                {
+                    { Length: 6 } and (_, _, _, d: false, _, _, _) => 0,
+                    { Length: 2 } => 1,
+                    { Length: 5 } and (_, b: false, _, _, _, f: false, _) => 2,
+                    { Length: 5 } and (_, b: false, _, _, e: false, _, _) => 3,
+                    { Length: 4 } => 4,
+                    { Length: 5 } and (_, _, c: false, _, e: false, _, _) => 5,
+                    { Length: 6 } and (_, _, c: false, _, _, _, _) => 6,
+                    { Length: 3 } => 7,
+                    { Length: 7 } => 8,
+                    { Length: 6 } and (_, _, _, _, e: false, _, _) => 9,
+                };
+            }
+            sum += value;
+        }
+        return sum;
     }
 }
