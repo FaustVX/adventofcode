f6ffcc70a006f1dbdfa72ebd5774fd7e01c1a3d0
FaustVX <jodu035@gmail.com>
2021-12-09 01:53:21 +0100

    Y2021D08 cleanup

--- 2021/Day08/Solution.cs
+++ 2021/Day08/Solution.cs
@@ -5,8 +5,6 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Text;
 using System.Runtime.InteropServices;
-using System.Collections.ObjectModel;
-using AdventOfCode.Y2020.Day19;
 
 namespace AdventOfCode.Y2021.Day08;
 
@@ -24,24 +22,24 @@ class Solution : Solver
             var remainingDigits = Patterns.ToList();
             var _1 = remainingDigits.Single(static digit => digit.Length is 2);
             remainingDigits.Remove(_1);
-            var _cOr_f = _1.Segments.ToList();
+            var _cOr_f = _1.ToList();
             var _7 = remainingDigits.Single(static digit => digit.Length is 3);
             remainingDigits.Remove(_7);
-            var _a = _7.Segments.Except(_1.Segments).Single();
+            var _a = _7.Except(_1).Single();
             var _4 = remainingDigits.Single(static digit => digit.Length is 4);
             remainingDigits.Remove(_4);
-            var _bOr_d = _4.Segments.Except(_1.Segments).ToList();
+            var _bOr_d = _4.Except(_1).ToList();
             var _abcdf = _bOr_d.Concat(_cOr_f).Append(_a).ToList();
             var _9 = remainingDigits.Where(static digit => digit.Length is 6)
-                .Single(digit => digit.Segments.Except(_abcdf).Count() is 1);
+                .Single(digit => digit.Except(_abcdf).Count() is 1);
             remainingDigits.Remove(_9);
-            var _g = _9.Segments.Except(_abcdf).Single();
+            var _g = _9.Except(_abcdf).Single();
             var _8 = remainingDigits.Single(static digit => digit.Length is 7);
             remainingDigits.Remove(_8);
-            var _e = _8.Segments.Except(_9.Segments).Single();
+            var _e = _8.Except(_9).Single();
             var _0Or_6 = remainingDigits.Where(static digit => digit.Length is 6).ToList();
-            var _cOr_d = _0Or_6[0].Segments.Except(_0Or_6[1].Segments).ToList();
-                _cOr_d.Add(_0Or_6[1].Segments.Except(_0Or_6[0].Segments).Single());
+            var _cOr_d = _0Or_6[0].Except(_0Or_6[1]).ToList();
+                _cOr_d.Add(_0Or_6[1].Except(_0Or_6[0]).Single());
             var _c = _cOr_d.Intersect(_cOr_f).Single();
             var _d = _cOr_d.Except(Enumerable.Repeat(_c, 1)).Single();
             var _f = _cOr_f.Except(Enumerable.Repeat(_c, 1)).Single();
@@ -68,7 +66,7 @@ class Solution : Solver
         }
     }
 
-    public class Digit
+    public class Digit : IEnumerable<char>
     {
         public char[] Segments { get; init; }
         public int Length => Segments.Length;
@@ -84,6 +82,12 @@ class Solution : Solver
 
         public void Deconstruct(out bool a, out bool b, out bool c, out bool d, out bool e, out bool f, out bool g)
             => (a, b, c, d, e, f, g) = (Segments.Contains('a'), Segments.Contains('b'), Segments.Contains('c'), Segments.Contains('d'), Segments.Contains('e'), Segments.Contains('f'), Segments.Contains('g'));
+
+        public IEnumerator<char> GetEnumerator()
+            => ((IEnumerable<char>)Segments).GetEnumerator();
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            => GetEnumerator();
     }
 
     private static IEnumerable<Entry> Parse(string input)
