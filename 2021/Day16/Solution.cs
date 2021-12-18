69799e5751548122def019ef43694ea12abe4987
FaustVX <jodu035@gmail.com>
2021-12-18 01:04:36 +0100

    Y2021D16P2 ( Don't work for my input P2)

--- 2021/Day16/Solution.cs
+++ 2021/Day16/Solution.cs
@@ -6,78 +6,32 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System.Numerics;
 using System.Globalization;
+using System.Diagnostics;
 
 namespace AdventOfCode.Y2021.Day16;
 
 [ProblemName("Packet Decoder")]
-class Solution : Solver
+partial class Solution : Solver
 {
-
-    public object PartOne(string input)
+    private static IPacket Parse(string input)
     {
         var packet = BigInteger.Parse(input, NumberStyles.HexNumber)
             .ToByteArray()
             .Reverse()
-            .Aggregate(new StringBuilder(), static (sb, b) => sb.Append(Convert.ToString(b, 2).PadLeft(8, '0')))
+            .Aggregate(new StringBuilder(), [DebuggerStepThrough] static (sb, b) => sb.Append(Convert.ToString(b, 2).PadLeft(8, '0')))
             .ToString();
-        return GetPacketsVersion(packet, out _);
+            return IPacket.Parse(packet, out _);
+    }
+    public object PartOne(string input)
+    {
+        return GetInner(Parse(input)).Sum([DebuggerStepThrough] static (p) => p.Version);
 
-        static int GetPacketsVersion(ReadOnlySpan<char> packet, out int readBits)
-        {
-            var version = ParseBinary(packet[..3]);
-            var type = ParseBinary(packet[3..6]);
-            if (type is 4)
-            {
-                var i = 6;
-                while (packet[i] is '1')
-                     i += 5;
-                readBits = i + 5;
-                return version;
-            }
-            readBits = 7;
-            switch (packet[6])
-            {
-                case '0':
-                {
-                    readBits += 15;
-                    var length = readBits + ParseBinary(packet[7..readBits]);
-                    while (readBits < length)
-                    {
-                        version += GetPacketsVersion(packet[readBits..], out var bits);
-                        readBits += bits;
-                    }
-                    return version;
-                }
-                case '1':
-                {
-                    readBits += 11;
-                    var length = ParseBinary(packet[7..readBits]);
-                    for (int i = 0; i < length; i++)
-                    {
-                        version += GetPacketsVersion(packet[readBits..], out var bits);
-                        readBits += bits;
-                    }
-                    return version;
-                }
-                default:
-                    throw new();
-            }
-        }
-
-        static int ParseBinary(ReadOnlySpan<char> input)
-        {
-            var value = 0;
-            foreach (var item in input)
-            {
-                value <<= 1;
-                value += item is '1' ? 1 : 0;
-            }
-            return value;
-        }
+        static IEnumerable<IPacket> GetInner(IPacket packet)
+            => packet is IOperatorPacket op
+                ? op.InnerPacket.SelectMany(GetInner).Append(packet)
+                : Enumerable.Repeat(packet, 1);
     }
 
     public object PartTwo(string input)
-    {
-        return 0;
-    }
+        => Parse(input).Evaluate();
 }
