50428a68e504da44f3b69a2c0fce6463fb631046
FaustVX <jodu035@gmail.com>
2021-12-19 17:51:10 +0100

    Added `DebuggerStepThrough` on StringExtensions methods

--- Lib/StringExtensions.cs
+++ Lib/StringExtensions.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode;
 
 public static class StringExtensions {
+    [DebuggerStepThrough]
     public static string StripMargin(this string st, string margin = "|") {
         return string.Join("\n",
             from line in st.SplitLine()
@@ -11,6 +13,7 @@ public static class StringExtensions {
         );
     }
 
+    [DebuggerStepThrough]
     public static string Indent(this string st, int l) {
         return string.Join("\n" + new string(' ', l),
             from line in st.SplitLine()
@@ -18,9 +21,11 @@ public static class StringExtensions {
         );
     }
 
+    [DebuggerStepThrough]
     public static string[] SplitLine(this string st)
         => Regex.Split(st, "\r?\n");
 
+    [DebuggerStepThrough]
     public static string[] Split2Lines(this string st)
         => Regex.Split(st, "\r?\n\r?\n");
 }
