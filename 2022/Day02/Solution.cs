802b855599b5f8602b9416d892d817ff74b89673
FaustVX <jodu035@gmail.com>
2022-12-02 06:23:10 +0100

    Solved P1

--- 2022/Day02/Solution.cs
+++ 2022/Day02/Solution.cs
@@ -6,7 +6,22 @@ class Solution : Solver
 
     public object PartOne(string input)
     {
-        return 0;
+        var sum = 0;
+        foreach (var rule in input.SplitLine())
+        {
+            // A X : Rock
+            // B Y : Paper
+            // C Z : Scissors
+
+            var me = rule[2] - 'X' + 1;
+            sum += me + rule switch
+            {
+                ['A', _, 'X'] or ['B', _, 'Y'] or ['C', _, 'Z'] => 3,
+                ['A', _, 'Y'] or ['B', _, 'Z'] or ['C', _, 'X'] => 6,
+                ['A', _, 'Z'] or ['B', _, 'X'] or ['C', _, 'Y'] => 0,
+            };
+        }
+        return sum;
     }
 
     public object PartTwo(string input)
