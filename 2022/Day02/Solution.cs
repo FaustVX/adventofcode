be9522430da80e3f6fc50dc53d127b526433de2b
FaustVX <jodu035@gmail.com>
2022-12-02 06:29:58 +0100

    Solved P1

--- 2022/Day02/Solution.cs
+++ 2022/Day02/Solution.cs
@@ -26,6 +26,40 @@ class Solution : Solver
 
     public object PartTwo(string input)
     {
-        return 0;
+        var sum = 0;
+        foreach (var rule in input.SplitLine())
+        {
+            // A : Rock
+            // B : Paper
+            // C : Scissors
+
+            var loose = rule[0] switch
+            {
+                'A' => 3,
+                'B' => 1,
+                'C' => 2,
+            };
+
+            var draw = rule[0] switch
+            {
+                'A' => 1,
+                'B' => 2,
+                'C' => 3,
+            };
+
+            var win = rule[0] switch
+            {
+                'A' => 2,
+                'B' => 3,
+                'C' => 1,
+            };
+            sum += rule switch
+            {
+                ['A', _, 'X'] or ['B', _, 'X'] or ['C', _, 'X'] => 0 + loose,
+                ['A', _, 'Y'] or ['B', _, 'Y'] or ['C', _, 'Y'] => 3 + draw,
+                ['A', _, 'Z'] or ['B', _, 'Z'] or ['C', _, 'Z'] => 6 + win,
+            };
+        }
+        return sum;
     }
 }
