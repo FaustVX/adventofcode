ecaf71f4a087a4b89e2913c222b6959c5f8be513
FaustVX <jodu035@gmail.com>
2022-12-02 22:01:35 +0100

    Refactored Y2022/D01

--- 2022/Day01/Solution.cs
+++ 2022/Day01/Solution.cs
@@ -3,7 +3,6 @@ namespace AdventOfCode.Y2022.Day01;
 [ProblemName("Calorie Counting")]
 class Solution : Solver
 {
-
     public object PartOne(string input)
         => GetFirsts(1, input);
 
@@ -13,22 +12,31 @@ class Solution : Solver
     private static int GetFirsts(int count, string input)
     {
         Span<int> firsts = stackalloc int[count];
-        var current = 0;
-        foreach (var line in (input.TrimEnd() + "\n\n").SplitLine())
+        Parse(firsts, input);
+        return CalculateSum(firsts);
+
+        static void Parse(Span<int> firsts, string input)
         {
-            if (line == "")
-            {
-                for (int i = 0; i < count; i++)
-                    if (current > firsts[i])
-                        (current, firsts[i]) = (firsts[i], current);
-                current = 0;
-                continue;
-            }
-            current += int.Parse(line);
+            var current = 0;
+            var count = firsts.Length;
+            foreach (var line in (input.TrimEnd() + "\n\n").SplitLine())
+                if (line == "")
+                {
+                    for (int i = 0; i < count; i++)
+                        if (current > firsts[i])
+                            (current, firsts[i]) = (firsts[i], current);
+                    current = 0;
+                }
+                else
+                    current += int.Parse(line);
         }
-        var sum = 0;
-        foreach (var first in firsts)
-            sum += first;
-        return sum;
+
+        static int CalculateSum(Span<int> firsts)
+        {
+            var sum = 0;
+            foreach (var first in firsts)
+                sum += first;
+            return sum;
+        }
     }
 }
