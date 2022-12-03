578070d406a926ccbac7999fbaf2abe95d103d44
FaustVX <jodu035@gmail.com>
2022-12-03 06:26:39 +0100

    Solved P1

--- 2022/Day03/Solution.cs
+++ 2022/Day03/Solution.cs
@@ -4,12 +4,25 @@ namespace AdventOfCode.Y2022.Day03;
 class Solution : Solver
 {
     public object PartOne(string input)
-    {
-        return 0;
-    }
+        => input.SplitLine()
+            .Select(FindCommonItem)
+            .Select(GetItemPriority)
+            .Sum();
 
     public object PartTwo(string input)
     {
         return 0;
     }
+
+    private static char FindCommonItem(string sack)
+    {
+        var length = sack.Length / 2;
+        foreach (var item in sack.AsSpan(0, length))
+            if (sack.AsSpan(length).Contains(item))
+                return item;
+        throw new();
+    }
+
+    private static int GetItemPriority(char item)
+        => item is >= 'a' ? item - 'a' + 1 : item - 'A' + 27;
 }
