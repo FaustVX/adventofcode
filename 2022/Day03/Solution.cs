6a3df7a68a7b441fe0e334b8b9b8c8ca03715ebd
FaustVX <jodu035@gmail.com>
2022-12-03 06:46:57 +0100

    P2 OneLiner

--- 2022/Day03/Solution.cs
+++ 2022/Day03/Solution.cs
@@ -10,17 +10,18 @@ class Solution : Solver
             .Sum();
 
     public object PartTwo(string input)
+        => input.SplitLine()
+            .Chunk(3)
+            .Select(GetBadgeItem)
+            .Select(GetItemPriority)
+            .Sum();
+
+    private static char GetBadgeItem(string[] items)
     {
-        var sum = 0;
-        var lines = input.SplitLine();
-        for (var i = 0; i < lines.Length; i += 3)
-            foreach (var item in lines[i])
-                if (lines[i+1].Contains(item) && lines[i+2].Contains(item))
-                {
-                    sum += GetItemPriority(item);
-                    break;
-                }
-        return sum;
+        foreach (var item in items[0])
+            if (items[1].Contains(item) && items[2].Contains(item))
+                return item;
+        throw new("No badge found");
     }
 
     private static char FindCommonItem(string sack)
@@ -29,7 +30,7 @@ class Solution : Solver
         foreach (var item in sack.AsSpan(0, length))
             if (sack.AsSpan(length).Contains(item))
                 return item;
-        throw new();
+        throw new("No item in common between the 2 compartments");
     }
 
     private static int GetItemPriority(char item)
