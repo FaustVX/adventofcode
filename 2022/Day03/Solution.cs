bfa7aac4637b4ffb2d6d65e082487fb0fc225965
FaustVX <jodu035@gmail.com>
2022-12-03 06:16:52 +0100

    Initial commit for Y2022D3

---
+++ 2022/Day03/Solution.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode.Y2022.Day03;
+
+[ProblemName("Rucksack Reorganization")]
+class Solution : Solver
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
