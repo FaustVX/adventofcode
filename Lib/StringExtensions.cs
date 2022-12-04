3272c781c8b570b0b738a631b3726bff6ea7b336
FaustVX <jodu035@gmail.com>
2022-12-05 00:37:28 +0100

    Added Display

--- Lib/StringExtensions.cs
+++ Lib/StringExtensions.cs
@@ -36,4 +36,35 @@ public static class StringExtensions {
                 Console.Write(letter);
             }
     }
+
+    [DebuggerStepThrough]
+    public static void TypeString(this IEnumerable<TypedString> input, TimeSpan maxTotalDuration)
+    {
+        var strings = input.ToArray();
+        var offset = maxTotalDuration / strings.Sum(static s => s.Input.Length);
+        var (fore, back) = (Console.ForegroundColor, Console.BackgroundColor);
+        foreach (var chunk in input)
+        {
+            if (chunk.Background is {} b)
+                Console.BackgroundColor = b;
+            if (chunk.Foregroung is {} f)
+                Console.ForegroundColor = f;
+            foreach (var letter in chunk.Input)
+            {
+                Thread.Sleep(Random.Shared.NextDouble() * offset);
+                Console.Write(letter);
+            }
+            if (chunk.Background is {})
+                Console.BackgroundColor = back;
+            if (chunk.Foregroung is {})
+                Console.ForegroundColor = fore;
+        }
+    }
+}
+
+public class TypedString
+{
+    public ConsoleColor? Foregroung { get; init; }
+    public ConsoleColor? Background { get; init; }
+    public required string Input { get; init; }
 }
