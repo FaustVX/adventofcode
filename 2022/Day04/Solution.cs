482eec5d981df6878a4339ad77b4845d25bddc26
FaustVX <jodu035@gmail.com>
2022-12-04 07:47:26 +0100

    Solved P1

--- 2022/Day04/Solution.cs
+++ 2022/Day04/Solution.cs
@@ -5,7 +5,15 @@ class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        var count = 0;
+        foreach (var assigment in input.SplitLine())
+        {
+            var sections = assigment.Split(',').SelectMany(static section => section.Split('-').Select(int.Parse)).ToArray();
+            var (startA, endA, startB, endB) = (sections[0], sections[1], sections[2], sections[3]);
+            if ((startA <= startB && endA >= endB) || (startA >= startB && endA <= endB))
+                count++;
+        }
+        return count;
     }
 
     public object PartTwo(string input)
