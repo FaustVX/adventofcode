a13c329c9366790fa740f3b64db96447c2da6dcf
FaustVX <jodu035@gmail.com>
2022-12-04 06:03:26 +0100

    Initial commit for Y2022D4

---
+++ 2022/Day04/Solution.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode.Y2022.Day04;
+
+[ProblemName("Camp Cleanup")]
+class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
