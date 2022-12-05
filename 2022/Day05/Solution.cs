156e462e505343054247cc39404f4c8e185ec412
FaustVX <jodu035@gmail.com>
2022-12-05 08:57:49 +0100

    Removed the `IEnumerator`

--- 2022/Day05/Solution.cs
+++ 2022/Day05/Solution.cs
@@ -5,46 +5,41 @@ class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        var enumerator = input.SplitLine().AsEnumerable().GetEnumerator();
-        var stacks = ParseStacks(enumerator);
-        foreach (var (qty, from, to) in ParseInstruction(enumerator))
+        var (map, (instructions, _)) = input.Split2Lines();
+        var stacks = ParseStacks(map.SplitLine());
+        foreach (var (qty, from, to) in ParseInstruction(instructions.SplitLine()))
             for (int i = 0; i < qty; i++)
                 stacks[to].Push(stacks[from].Pop());
         return string.Concat(stacks.Select(static stack => stack.Peek()));
     }
 
-    Stack<char>[] ParseStacks(IEnumerator<string> input)
+    Stack<char>[] ParseStacks(string[] lines)
     {
-        input.MoveNext();
-        var capacity = (input.Current.Length + 1) / 4;
+        var capacity = (lines[0].Length + 1) / 4;
         var stacks = new Stack<char>[capacity];
         for (int i = 0; i < capacity; i++)
             stacks[i] = new();
 
-        for (var line = input.Current; input.MoveNext(); line = input.Current)
+        foreach (var line in lines)
             for (int i = 0; i < capacity; i++)
                 switch (line.AsSpan(i * 4, 3))
                 {
-                    case [' ', ' ', ' ']:
-                        continue;
-                    case ['[', >= 'A' and <= 'Z' and var c, ']']:
+                    case ['[', var c, ']']:
                         stacks[i].Push(c);
                         break;
-                    default:
+                    case not [' ', ' ', ' ']:
                         for (int j = 0; j < capacity; j++)
                             stacks[j] = new(stacks[j]); // Reverse the stacks
-                        input.MoveNext();
                         return stacks;
                 }
-        throw new();
+        throw new UnreachableException();
     }
 
-    IEnumerable<(int qty, int from, int to)> ParseInstruction(IEnumerator<string> input)
+    IEnumerable<(int qty, int from, int to)> ParseInstruction(string[] input)
     {
-        do
+        for (int i = 0; i < input.Length; i++)
         {
-            var line = input.Current.AsMemory();
-            line = line[5..];
+            var line = input[i].AsMemory(5);
             var space = line.Span.IndexOf(' ');
             var qty = int.Parse(line[..space].Span);
             line = line[(space + 6)..];
@@ -53,14 +48,14 @@ class Solution : Solver //, IDisplay
             line = line[(space + 4)..];
             var to = int.Parse(line.Span) - 1;
             yield return (qty, from, to);
-        } while (input.MoveNext());
+        }
     }
 
     public object PartTwo(string input)
     {
-        var enumerator = input.SplitLine().AsEnumerable().GetEnumerator();
-        var stacks = ParseStacks(enumerator);
-        foreach (var (qty, from, to) in ParseInstruction(enumerator))
+        var (map, (instructions, _)) = input.Split2Lines();
+        var stacks = ParseStacks(map.SplitLine());
+        foreach (var (qty, from, to) in ParseInstruction(instructions.SplitLine()))
             if (qty == 1)
                 stacks[to].Push(stacks[from].Pop());
             else
