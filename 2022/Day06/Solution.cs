df0cda3b075e5b327c00fcdbaa89c739588b2e13
FaustVX <jodu035@gmail.com>
2022-12-06 06:04:05 +0100

    Initial commit for Y2022D6

---
+++ 2022/Day06/Solution.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode.Y2022.Day06;
+
+[ProblemName("Tuning Trouble")]
+class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
