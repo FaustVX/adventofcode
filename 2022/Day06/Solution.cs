fa79d4e78a31c07c29c539604440bdcf45fe7999
FaustVX <jodu035@gmail.com>
2022-12-06 06:11:22 +0100

    Solved P1 in 0:07:17

--- 2022/Day06/Solution.cs
+++ 2022/Day06/Solution.cs
@@ -5,7 +5,19 @@ class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        for (int i = 0; i < input.Length; i++)
+            if (IsUnique(input.AsSpan(i, 4)))
+                return i + 4;
+        throw new UnreachableException();
+
+        bool IsUnique(ReadOnlySpan<char> span)
+        {
+            var set = new HashSet<char>(4);
+            foreach (var letter in span)
+                if (!set.Add(letter))
+                    return false;
+            return true;
+        }
     }
 
     public object PartTwo(string input)
