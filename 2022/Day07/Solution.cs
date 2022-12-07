f6c642f9619decffdcd0fd224fc64f6051d743c2
FaustVX <jodu035@gmail.com>
2022-12-07 06:04:55 +0100

    Initial commit for Y2022D7

---
+++ 2022/Day07/Solution.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode.Y2022.Day07;
+
+[ProblemName("No Space Left On Device")]
+class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
