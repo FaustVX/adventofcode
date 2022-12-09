2dc3d07298757ba9f5ad1c62c45ae3836ec90355
FaustVX <jodu035@gmail.com>
2022-12-09 08:49:19 +0100

    Solved P1 in 2:44:46

--- 2022/Day09/Solution.cs
+++ 2022/Day09/Solution.cs
@@ -5,7 +5,37 @@ public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        var head = (x: 0, y: 0);
+        var tail = head;
+        var visited = new HashSet<(int x, int y)>()
+        {
+            tail,
+        };
+        foreach (var move in input.AsMemory().SplitLine())
+        {
+            var dir = move.Span[0] switch
+            {
+                'U' => (0, -1),
+                'D' => (0, 1),
+                'L' => (-1, 0),
+                'R' => (1, 0),
+                _ => throw new UnreachableException(),
+            };
+            for (var i = int.Parse(move.Span[2..]) - 1; i >= 0; i--)
+                Move(ref head, ref tail, visited, dir);
+        }
+        return visited.Count;
+    }
+
+    void Move(ref (int x, int y) head, ref (int x, int y) tail, HashSet<(int x, int y)> visited, (int x, int y) dir)
+    {
+        var previousHead = head;
+        head.x += dir.x;
+        head.y += dir.y;
+        if (head.x - tail.x is >= -1 and <= 1 && head.y - tail.y is >= -1 and <= 1)
+            return;
+        tail = previousHead;
+        visited.Add(tail);
     }
 
     public object PartTwo(string input)
