d5d136d281f1d006badc60b36f7669cefa16a22a
FaustVX <jodu035@gmail.com>
2022-12-09 06:04:33 +0100

    Initial commit for Y2022D9

---
+++ 2022/Day09/Solution.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode.Y2022.Day09;
+
+[ProblemName("Rope Bridge")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
