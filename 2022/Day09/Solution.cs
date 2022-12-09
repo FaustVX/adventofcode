73b43afde5c43ef3ad29838def929a8b1651ec95
FaustVX <jodu035@gmail.com>
2022-12-09 13:58:30 +0100

    Refactored to a single method

--- 2022/Day09/Solution.cs
+++ 2022/Day09/Solution.cs
@@ -4,44 +4,15 @@ namespace AdventOfCode.Y2022.Day09;
 public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
-    {
-        var head = (x: 0, y: 0);
-        var tail = head;
-        var visited = new HashSet<(int x, int y)>()
-        {
-            tail,
-        };
-        foreach (var move in input.AsMemory().SplitLine())
-        {
-            var dir = move.Span[0] switch
-            {
-                'U' => (0, -1),
-                'D' => (0, 1),
-                'L' => (-1, 0),
-                'R' => (1, 0),
-                _ => throw new UnreachableException(),
-            };
-            for (var i = int.Parse(move.Span[2..]) - 1; i >= 0; i--)
-                Move(ref head, ref tail, visited, dir);
-        }
-        return visited.Count;
-
-        static void Move(ref (int x, int y) head, ref (int x, int y) tail, HashSet<(int x, int y)> visited, (int x, int y) dir)
-        {
-            var previousHead = head;
-            head.x += dir.x;
-            head.y += dir.y;
-            if (head.x - tail.x is >= -1 and <= 1 && head.y - tail.y is >= -1 and <= 1)
-                return;
-            tail = previousHead;
-            visited.Add(tail);
-        }
-    }
+    => Execute(input, 2).Count;
 
     public object PartTwo(string input)
+    => Execute(input, 10).Count;
+
+    private static HashSet<(int x, int y)> Execute(string input, int ropeLength)
     {
-        Span<(int x, int y)> rope = stackalloc (int, int)[10];
-        var visited = new HashSet<(int x, int y)>()
+        Span<(int x, int y)> rope = stackalloc (int, int)[ropeLength];
+        var visited = new HashSet<(int x, int y)>(capacity: 1300)
         {
             (0, 0),
         };
@@ -61,26 +32,26 @@ public class Solution : Solver //, IDisplay
                 visited.Add(rope[^1]);
             }
         }
-        return visited.Count;
+        return visited;
 
         static void Move(ref (int x, int y) head, Span<(int x, int y)> tail, (int x, int y) dir)
         {
-            if (dir is (0, 0))
-                return;
-            var knot = head;
             head.x += dir.x;
             head.y += dir.y;
             if (tail.IsEmpty)
                 return;
-            if (head.x - tail[0].x is >= -1 and <= 1 && head.y - tail[0].y is >= -1 and <= 1)
-                return;
-            dir.x = (head.x - tail[0].x) switch
+            var offset = (x: head.x - tail[0].x, y: head.y - tail[0].y);
+            if (offset.x is >= -1 and <= 1)
+                if (offset.y is >= -1 and <= 1)
+                    return;
+
+            dir.x = offset.x switch
             {
                 > 0 => 1,
                 < 0 => -1,
                 _ => 0,
             };
-            dir.y = (head.y - tail[0].y) switch
+            dir.y = offset.y switch
             {
                 > 0 => 1,
                 < 0 => -1,
