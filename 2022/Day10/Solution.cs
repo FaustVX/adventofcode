47e64654366d210c8e79eeb136608d1c1f427d30
FaustVX <jodu035@gmail.com>
2022-12-10 06:03:10 +0100

    Initial commit for Y2022D10

---
+++ 2022/Day10/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2022.Day10;
+
+[ProblemName("Cathode-Ray Tube")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
