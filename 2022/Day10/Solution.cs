c430c8c7996a6f94650b9b8cc7c88a0c5536afcc
FaustVX <jodu035@gmail.com>
2022-12-10 07:11:01 +0100

    Solved P2 in 0:28:10

--- 2022/Day10/Solution.cs
+++ 2022/Day10/Solution.cs
@@ -19,6 +19,32 @@ public class Solution : Solver //, IDisplay
 
     public object PartTwo(string input)
     {
-        return 0;
+        var screen = new long[6];
+        var cpu = new CPU(input.AsMemory().SplitLine());
+        foreach (var cycle in cpu.Run())
+        {
+            var line = Math.DivRem(cycle - 1, 40, out var index);
+            if (index >= cpu.X - 1 && index <= cpu.X + 1)
+                SetPixel(screen, index, line);
+        }
+        for (int y = 0; y < screen.Length; y++)
+        {
+            for (int x = 0; x < 40; x++)
+                Console.Write(GetPixel(screen, x, y) ? '#' : ' ');
+            Console.WriteLine();
+        }
+        return Console.ReadLine()!;
+
+        static void SetPixel(long[] screen, int x, int y)
+        {
+            ref var line = ref screen[y];
+            line |= 1L << x;
+        }
+
+        static bool GetPixel(long[] screen, int x, int y)
+        {
+            var line = screen[y];
+            return (line & (1L << x)) != 0;
+        }
     }
 }
