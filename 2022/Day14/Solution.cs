9b6f083c75ca906d7a2db1fd06e7c9097fd71df3
FaustVX <jodu035@gmail.com>
2022-12-14 09:56:14 +0100

    Initial commit for Y2022D14

---
+++ 2022/Day14/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2022.Day14;
+
+[ProblemName("Regolith Reservoir")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
