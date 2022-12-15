7b0dd394e999047980e6bcbc5ba5b212484e41a0
FaustVX <jodu035@gmail.com>
2022-12-15 11:10:18 +0100

    Solved P1 in 2:19:09

--- 2022/Day15/Solution.cs
+++ 2022/Day15/Solution.cs
@@ -6,7 +6,31 @@ public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        var cave = Cave.Parse(input.AsMemory());
+        return Enumerable.Repeat(cave, cave.Size.x).Where(cave.Size.y <= 100 ? GetForTest : GetForInput).Count();
+
+        static bool GetForTest(Cave c, int x)
+        {
+            switch (c[x + c.Offset.x, 10])
+            {
+                case Location.NotBeacon:
+                    Console.Write('#');
+                    return true;
+                case Location.Empty:
+                    Console.Write('.');
+                    break;
+                case Location.Sensor:
+                    Console.Write('S');
+                    break;
+                case Location.Beacon:
+                    Console.Write('B');
+                    break;
+            }
+            return false;
+        }
+
+        static bool GetForInput(Cave c, int x)
+        => c[x + c.Offset.x, 2_000_000] is Location.NotBeacon;
     }
 
     public object PartTwo(string input)
