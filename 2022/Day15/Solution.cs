50244848efeacf1f57b1a201dee98dd45bfebe29
FaustVX <jodu035@gmail.com>
2022-12-15 08:51:09 +0100

    Initial commit for Y2022D15

---
+++ 2022/Day15/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2022.Day15;
+
+[ProblemName("Beacon Exclusion Zone")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
