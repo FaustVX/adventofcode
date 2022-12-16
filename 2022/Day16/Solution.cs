0a06528262d3aa32d47b64c9eab4452f958535e5
FaustVX <jodu035@gmail.com>
2022-12-16 08:53:58 +0100

    Initial commit for Y2022D16

---
+++ 2022/Day16/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2022.Day16;
+
+[ProblemName("Proboscidea Volcanium")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
