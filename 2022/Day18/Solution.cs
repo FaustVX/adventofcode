a2dd13c2d4f5f09c9349add4f69cb42433d40dd8
FaustVX <jodu035@gmail.com>
2022-12-18 12:23:27 +0100

    Solved P1 in 0:24:40

--- 2022/Day18/Solution.cs
+++ 2022/Day18/Solution.cs
@@ -6,7 +6,30 @@ public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        var voxels = ParseVoxels(input.AsMemory().SplitLine());
+        ReadOnlySpan<(int x, int y, int z)> dirs = stackalloc (int, int, int)[]
+        {
+            (-1, 0, 0),
+            (+1, 0, 0),
+            (0, -1, 0),
+            (0, +1, 0),
+            (0, 0, -1),
+            (0, 0, +1),
+        };
+        var emptyFaces = 0;
+        foreach (var voxel in voxels)
+            foreach (var dir in dirs)
+                if (!voxels.Contains(voxel.Add(dir)))
+                    emptyFaces++;
+        return emptyFaces;
+    }
+
+    private static HashSet<(int x, int y, int z)> ParseVoxels(ReadOnlyMemory<ReadOnlyMemory<char>> positions)
+    {
+        var voxels = new HashSet<(int x, int y, int z)>(capacity: positions.Length);
+        foreach (var position in positions.Span)
+            voxels.Add(Extensions.ParseToTuple<int, int, int>(position.Split(",")));
+        return voxels;
     }
 
     public object PartTwo(string input)
