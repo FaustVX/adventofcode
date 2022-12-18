a384486afc93eb4bec437d07b007f5bf9554767c
FaustVX <jodu035@gmail.com>
2022-12-18 19:56:40 +0100

    Using BFS Still too slow

--- 2022/Day18/Solution.cs
+++ 2022/Day18/Solution.cs
@@ -32,43 +32,10 @@ public class Solution : Solver //, IDisplay
         return voxels;
     }
 
-    static void CalculateP2((int x, int y, int z) xyz, HashSet<(int x, int y, int z)> voxels, List<LinkedList<(int x, int y, int z)>> groups, ReadOnlySpan<(int, int, int)> dirs)
-    {
-        var isInVoxels = voxels.Contains(xyz);
-        if (isInVoxels || groups.Any(g => g.Contains(xyz)))
-            return;
-
-        var group = default(LinkedList<(int x, int y, int z)>);
-        foreach (var dir in dirs)
-        {
-            var pos = xyz.Add(dir);
-            if (voxels.Contains(pos) == isInVoxels)
-            {
-                var g = groups.FirstOrDefault(g => g.Contains(pos));
-                if ((g?.Count ?? 0) > (group?.Count ?? 0))
-                    if (group is null)
-                        group = g;
-                    else if (g is not null)
-                    {
-                        foreach (var item in g)
-                            group.AddLast(item);
-                        groups.Remove(g);
-                    }
-            }
-        }
-
-        if (group is null)
-        {
-            group = new();
-            groups.Add(group);
-        }
-        group.AddLast(xyz);
-    }
-
     public object PartTwo(string input)
     {
-        var voxels = ParseVoxels(input.AsMemory().SplitLine());
-        var (minX, maxX, minY, maxY, minZ, maxZ) = voxels.GetMinMax();
+        var magmas = ParseVoxels(input.AsMemory().SplitLine());
+        var (minX, maxX, minY, maxY, minZ, maxZ) = magmas.GetMinMax();
         var dirs = new (int, int, int)[]
         {
             (-1, 0, 0),
@@ -78,12 +45,42 @@ public class Solution : Solver //, IDisplay
             (0, 0, -1),
             (0, 0, +1),
         };
-        var groups = new List<LinkedList<(int x, int y, int z)>>();
+        var toCheck = new List<(int x, int y, int z)>() // will contains only air voxels
+        {
+            (minX, minY, minZ),
+            (minX, maxY, minZ),
+            (maxX, minY, minZ),
+            (maxX, maxY, minZ),
+            (minX, minY, maxZ),
+            (minX, maxY, maxZ),
+            (maxX, minY, maxZ),
+            (maxX, maxY, maxZ),
+        };
+        var airs = new HashSet<(int x, int y, int z)>(capacity: (maxX - minX + 1) * (maxY - minY + 1) * (maxZ - minZ + 1) - magmas.Count);
         for (var x = minX; x <= maxX; x++)
             for (var y = minY; y <= maxY; y++)
                 for (var z = minZ; z <= maxZ; z++)
-                    CalculateP2((x, y, z), voxels, groups, dirs);
+                    if (!magmas.Contains((x, y, z)))
+                        airs.Add((x, y, z));
+        while (toCheck.Count > 0)
+            for (var i = toCheck.Count - 1; i >= 0 ; i--)
+            {
+                var voxel = toCheck[i];
+                toCheck.Remove(voxel);
+                airs.Remove(voxel);
+                foreach (var dir in dirs)
+                {
+                    var pos = voxel.Add(dir);
+                    if (airs.Contains(pos))
+                        toCheck.Add(pos);
+                }
+            }
 
-        return voxels.SelectMany(voxel => dirs.Select(dir => voxel.Add(dir))).Count(groups.First(g => g.Contains((minX, minY, minZ))).Contains);
+        var emptyFaces = 0;
+        foreach (var voxel in magmas)
+            foreach (var dir in dirs)
+                if (!magmas.Contains(voxel.Add(dir)) && ! airs.Contains(voxel.Add(dir)))
+                    emptyFaces++;
+        return emptyFaces;
     }
 }
