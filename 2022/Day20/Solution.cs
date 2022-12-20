2c360aa31bf0c34dc5453981224fdb9aebbfb7b1
FaustVX <jodu035@gmail.com>
2022-12-20 20:10:12 +0100

    Initial commit for Y2022D20

---
+++ 2022/Day20/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2022.Day20;
+
+[ProblemName("Grove Positioning System")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
