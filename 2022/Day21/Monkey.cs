96cc740f8579227926ffea5456183ab9fb36ef51
FaustVX <jodu035@gmail.com>
2022-12-21 13:23:12 +0100

    Merged `OperationMonkey` with its `Builder`

--- 2022/Day21/Monkey.cs
+++ 2022/Day21/Monkey.cs
@@ -17,17 +17,15 @@ interface IMonkey
                 var leftName = line.Slice(6, 4).ToString();
                 var op = line.Span[11];
                 var rightName = line.Slice(13, 4).ToString();
-                cache[name] = new OperationMonkey.Builder()
+                cache[name] = new OperationMonkey()
                 {
                     Left = leftName,
                     Operation = op,
                     Right = rightName,
+                    Cache = cache,
                 };
             }
         }
-        foreach (var name in cache.Keys)
-            if (cache[name] is OperationMonkey.Builder b)
-                cache[name] = b.Build(cache);
         return cache;
     }
 }
@@ -39,31 +37,18 @@ sealed class NumberMonkey : IMonkey
 
 class OperationMonkey : IMonkey
 {
-    public class Builder : IMonkey
-    {
-        public required string Left { get; init; }
-        public required char Operation { get; init; }
-        public required string Right { get; init; }
-        public long Value => throw new NotImplementedException();
-        public IMonkey Build(IDictionary<string, IMonkey> cache)
-        => new OperationMonkey()
-        {
-            Left = cache[Left] is Builder l ? (cache[Left] = l.Build(cache)) : cache[Left],
-            Operation = Operation,
-            Right = cache[Right] is Builder r ? (cache[Right] = r.Build(cache)) : cache[Right],
-        };
-    }
-    public required IMonkey Left { get; init; }
+    public required string Left { get; init; }
     public required char Operation { get; init; }
-    public required IMonkey Right { get; init; }
+    public required string Right { get; init; }
+    public required IReadOnlyDictionary<string, IMonkey> Cache { get; init; }
     private long? _value;
     public long Value
     => _value ??= Operation switch
     {
-        '+' => Left.Value + Right.Value,
-        '-' => Left.Value - Right.Value,
-        '*' => Left.Value * Right.Value,
-        '/' => Left.Value / Right.Value,
+        '+' => Cache[Left].Value + Cache[Right].Value,
+        '-' => Cache[Left].Value - Cache[Right].Value,
+        '*' => Cache[Left].Value * Cache[Right].Value,
+        '/' => Cache[Left].Value / Cache[Right].Value,
         _ => throw new UnreachableException(),
     };
 }
