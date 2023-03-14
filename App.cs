dfb293874202de36d4804b1e66b425446622dbfa
FaustVX <jodu035@gmail.com>
2023-03-15 00:35:39 +0100

    Move `Usage.Get` to `ProjectReadmeGenerator`

--- App.cs
+++ App.cs
@@ -116,39 +116,3 @@ record class DayParameters([Argument]string date = "today") : ICommandParameterS
         return int.Parse(day.AsSpan(5));
     }
 }
-
-static class Usage {
-    public static string Get()
-        => """
-            Usage: dotnet run [arguments]
-            1) To run the solutions and admire your advent calendar:
-
-            [year]/[day|all]                                   Solve the specified problems
-            today                                              Shortcut to the above
-            [year]                                             Solve the whole year
-            all                                                Solve everything
-
-            calendars                                          Show the calendars
-
-            init [this .git repo] [sslSalt] ([sslPassword])    Initialize the current folder
-
-            2) To start working on new problems:
-            login to https://adventofcode.com, then copy your session cookie, and export
-            it in your console like this
-
-             export SESSION=73a37e9a72a...
-
-            then run the app with
-
-             update [year]/[day]   Prepares a folder for the given day, updates the input,
-                                   the readme and creates a solution template.
-             update today          Shortcut to the above.
-
-            3) To upload your answer:
-            set up your SESSION variable as above.
-
-             upload [year]/[day]   Upload the answer for the selected year and day.
-             upload today          Shortcut to the above.
-
-            """;
-}
--- Lib/Generator/ReadmeGenerator.cs
+++ Lib/Generator/ReadmeGenerator.cs
@@ -45,7 +45,7 @@ To run the project:
 2. Clone the repo
 3. Get help with `dotnet run`
 ```
-{Usage.Get()}
+{Usage()}
 ```
 
 ## Working in Visual Studio Code
@@ -58,6 +58,40 @@ Work on part 1. Check the solution with the `upload today` task. Continue with p
 **Note:** this feature relies on the "Memento Inputs" extension to store your session cookie, you need
 to set it up in advance from the Command Palette with `Install Extensions`.
 """;
+
+private static string Usage()
+=> """
+    Usage: dotnet run [arguments]
+    1) To run the solutions and admire your advent calendar:
+
+    [year]/[day|all]                                   Solve the specified problems
+    today                                              Shortcut to the above
+    [year]                                             Solve the whole year
+    all                                                Solve everything
+
+    calendars                                          Show the calendars
+
+    init [this .git repo] [sslSalt] ([sslPassword])    Initialize the current folder
+
+    2) To start working on new problems:
+    login to https://adventofcode.com, then copy your session cookie, and export
+    it in your console like this
+
+        export SESSION=73a37e9a72a...
+
+    then run the app with
+
+        update [year]/[day]   Prepares a folder for the given day, updates the input,
+                            the readme and creates a solution template.
+        update today          Shortcut to the above.
+
+    3) To upload your answer:
+    set up your SESSION variable as above.
+
+        upload [year]/[day]   Upload the answer for the selected year and day.
+        upload today          Shortcut to the above.
+
+    """;
 }
 
 static class ReadmeGeneratorForYear {
