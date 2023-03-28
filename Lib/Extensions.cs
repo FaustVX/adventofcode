928221f690f8c3a743c413a7cede8ff238260fde
FaustVX <jodu035@gmail.com>
2023-03-28 18:44:02 +0200

    Added `TupleExtensions`

--- Lib/Extensions.cs
+++ Lib/Extensions.cs
@@ -128,29 +128,6 @@ public static class Extensions
     }
 
     [DebuggerStepThrough]
-    public static (T, T) Add<T>(this (T, T) left, (T, T) right)
-    where T : System.Numerics.IAdditionOperators<T, T, T>
-    => (left.Item1 + right.Item1, left.Item2 + right.Item2);
-
-    [DebuggerStepThrough]
-    public static (T, T, T) Add<T>(this (T, T, T) left, (T, T, T) right)
-    where T : System.Numerics.IAdditionOperators<T, T, T>
-    => (left.Item1 + right.Item1, left.Item2 + right.Item2, left.Item3 + right.Item3);
-
-    [DebuggerStepThrough]
-    public static (T1 x, T2 y) ParseToTuple<T1, T2>(this ReadOnlyMemory<ReadOnlyMemory<char>> memory)
-    where T1 : ISpanParsable<T1>
-    where T2 : ISpanParsable<T2>
-    => (T1.Parse(memory.Span[0].Span, default), T2.Parse(memory.Span[1].Span, default));
-
-    [DebuggerStepThrough]
-    public static (T1 x, T2 y, T3 z) ParseToTuple<T1, T2, T3>(this ReadOnlyMemory<ReadOnlyMemory<char>> memory)
-    where T1 : ISpanParsable<T1>
-    where T2 : ISpanParsable<T2>
-    where T3 : ISpanParsable<T3>
-    => (T1.Parse(memory.Span[0].Span, default), T2.Parse(memory.Span[1].Span, default), T3.Parse(memory.Span[2].Span, default));
-
-    [DebuggerStepThrough]
     public static void SetMinMax<T>(this T value, ref T min, ref T max)
     where T : System.Numerics.IComparisonOperators<T, T, bool>
     {
---
+++ Lib/TupleExtensions.cs
@@ -0,0 +1,37 @@
+namespace AdventOfCode;
+
+public static class TupleExtensions
+{
+    [DebuggerStepThrough]
+    public static (T, T) Add<T>(this (T, T) left, (T, T) right)
+    where T : System.Numerics.IAdditionOperators<T, T, T>
+    => (left.Item1 + right.Item1, left.Item2 + right.Item2);
+
+    [DebuggerStepThrough]
+    public static (T, T, T) Add<T>(this (T, T, T) left, (T, T, T) right)
+    where T : System.Numerics.IAdditionOperators<T, T, T>
+    => (left.Item1 + right.Item1, left.Item2 + right.Item2, left.Item3 + right.Item3);
+
+    [DebuggerStepThrough]
+    public static (T, T) Times<T>(this (T, T) tuple, T factor)
+    where T : System.Numerics.IMultiplyOperators<T, T, T>
+    => (tuple.Item1 * factor, tuple.Item2 * factor);
+
+    [DebuggerStepThrough]
+    public static (T, T, T) Times<T>(this (T, T, T) tuple, T factor)
+    where T : System.Numerics.IMultiplyOperators<T, T, T>
+    => (tuple.Item1 * factor, tuple.Item2 * factor, tuple.Item3 * factor);
+
+    [DebuggerStepThrough]
+    public static (T1 x, T2 y) ParseToTuple<T1, T2>(this ReadOnlyMemory<ReadOnlyMemory<char>> memory)
+    where T1 : ISpanParsable<T1>
+    where T2 : ISpanParsable<T2>
+    => (T1.Parse(memory.Span[0].Span, default), T2.Parse(memory.Span[1].Span, default));
+
+    [DebuggerStepThrough]
+    public static (T1 x, T2 y, T3 z) ParseToTuple<T1, T2, T3>(this ReadOnlyMemory<ReadOnlyMemory<char>> memory)
+    where T1 : ISpanParsable<T1>
+    where T2 : ISpanParsable<T2>
+    where T3 : ISpanParsable<T3>
+    => (T1.Parse(memory.Span[0].Span, default), T2.Parse(memory.Span[1].Span, default), T3.Parse(memory.Span[2].Span, default));
+}
