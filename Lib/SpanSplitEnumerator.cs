c08cb44cc026be31f3215b80523ca6471f750fea
FaustVX <jodu035@gmail.com>
2023-04-03 19:51:50 +0200

    Updated `SpanSplitEnumerator` to allow list pattern matching

--- Lib/SpanSplitEnumerator.cs
+++ Lib/SpanSplitEnumerator.cs
@@ -5,68 +5,100 @@ namespace AdventOfCode;
 [DebuggerStepThrough]
 public ref struct SpanSplitEnumerator
 {
-    private ReadOnlySpan<char> _remaining;
+    private ReadOnlySpan<char> _buffer;
     private readonly ReadOnlySpan<char> _separator;
     private readonly bool _separateOnAny;
-    private ReadOnlySpan<char> _current;
-    private bool _isEnumeratorActive;
 
     public SpanSplitEnumerator(ReadOnlySpan<char> buffer, ReadOnlySpan<char> separator, bool separateOnAny)
     {
-        _remaining = buffer;
+        _buffer = buffer;
         _separator = separator;
         _separateOnAny = separateOnAny;
-        _current = default;
-        _isEnumeratorActive = true;
     }
 
     public SpanSplitEnumerator(ReadOnlySpan<char> buffer, ReadOnlySpan<char> separator)
+    : this(buffer, separator, false)
+    { }
+
+    public int Length
     {
-        _remaining = buffer;
-        _separator = separator;
-        _separateOnAny = false;
-        _current = default;
-        _isEnumeratorActive = true;
+        get
+        {
+            var count = 0;
+            var e = GetEnumerator();
+            while (e.MoveNext())
+                count++;
+            return count;
+        }
     }
 
-    /// <summary>
-    /// Gets the line at the current position of the enumerator.
-    /// </summary>
-    public ReadOnlySpan<char> Current => _current;
+    public Enumerator GetEnumerator()
+    => new(this);
 
-    /// <summary>
-    /// Returns this instance as an enumerator.
-    /// </summary>
-    public SpanSplitEnumerator GetEnumerator() => this;
-
-    /// <summary>
-    /// Advances the enumerator to the next line of the span.
-    /// </summary>
-    /// <returns>
-    /// True if the enumerator successfully advanced to the next line; false if
-    /// the enumerator has advanced past the end of the span.
-    /// </returns>
-    public bool MoveNext()
+    public ReadOnlySpan<char> this[int index]
     {
-        if (!_isEnumeratorActive)
-            return false; // EOF previously reached or enumerator was never initialized
-
-        var idx = _separateOnAny ? _remaining.IndexOfAny(_separator) : _remaining.IndexOf(_separator);
-
-        if ((uint)idx < (uint)_remaining.Length)
+        get
         {
-            _current = _remaining.Slice(0, idx);
-            _remaining = _remaining.Slice(idx + (_separateOnAny ? 1 : _separator.Length));
+            var e = GetEnumerator();
+            e.MoveNext();
+            for (var i = 0; i < index; i++)
+                if (!e.MoveNext())
+                    throw new IndexOutOfRangeException();
+            return e.Current;
         }
-        else
+    }
+
+    public ref struct Enumerator
+    {
+        private ReadOnlySpan<char> _remaining;
+        private readonly ReadOnlySpan<char> _separator;
+        private readonly bool _separateOnAny;
+        private ReadOnlySpan<char> _current;
+        private bool _isEnumeratorActive;
+
+        public Enumerator(SpanSplitEnumerator enumerator)
         {
-            // We've reached EOF, but we still need to return 'true' for this final
-            // iteration so that the caller can query the Current property once more.
-            _current = _remaining;
-            _remaining = default;
-            _isEnumeratorActive = false;
+            _remaining = enumerator._buffer;
+            _separator = enumerator._separator;
+            _separateOnAny = enumerator._separateOnAny;
+            _current = default;
+            _isEnumeratorActive = true;
         }
 
-        return true;
+        /// <summary>
+        /// Gets the line at the current position of the enumerator.
+        /// </summary>
+        public ReadOnlySpan<char> Current => _current;
+
+        /// <summary>
+        /// Advances the enumerator to the next line of the span.
+        /// </summary>
+        /// <returns>
+        /// True if the enumerator successfully advanced to the next line; false if
+        /// the enumerator has advanced past the end of the span.
+        /// </returns>
+        public bool MoveNext()
+        {
+            if (!_isEnumeratorActive)
+                return false; // EOF previously reached or enumerator was never initialized
+
+            var idx = _separateOnAny ? _remaining.IndexOfAny(_separator) : _remaining.IndexOf(_separator);
+
+            if ((uint)idx < (uint)_remaining.Length)
+            {
+                _current = _remaining.Slice(0, idx);
+                _remaining = _remaining.Slice(idx + (_separateOnAny ? 1 : _separator.Length));
+            }
+            else
+            {
+                // We've reached EOF, but we still need to return 'true' for this final
+                // iteration so that the caller can query the Current property once more.
+                _current = _remaining;
+                _remaining = default;
+                _isEnumeratorActive = false;
+            }
+
+            return true;
+        }
     }
 }
