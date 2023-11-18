3ba7173fb2d537b2ae0d1aa13d2b1ab335963b3e
FaustVX <jodu035@gmail.com>
2023-11-18 21:07:50 +0100

    Emit generated files

---
+++ Generated/PrimaryParameter.SG/PrimaryParameter.SG.Generator/FaustVX.PrimaryParameter.SG.g.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode.Model
+{
+    partial class Project
+    {
+        private readonly string _repo = repo;
+    }
+}
+namespace AdventOfCode.Model
+{
+    partial class Project
+    {
+        private readonly string _sslSalt = sslSalt;
+    }
+}
+namespace AdventOfCode.Model
+{
+    partial class Project
+    {
+        private readonly string _sslPassword = sslPassword;
+    }
+}
