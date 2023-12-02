971532a88fdca08cda7076bbf821209d93d830e2
FaustVX <jodu035@gmail.com>
2023-12-02 20:59:27 +0100

    Added a property to specify the `ArrayPool` generator

--- Lib/ArrayRental.cs
+++ Lib/ArrayRental.cs
@@ -7,12 +7,13 @@ namespace AdventOfCode;
 [StructLayout(LayoutKind.Auto)]
 public readonly partial struct ArrayRental<T>([Field]T[] array, [Field]int length)
 {
-    private static readonly ArrayPool<T> _pool = ArrayPool<T>.Create(10, 1);
+    public static Func<ArrayPool<T>> PoolGenerator { get; set; } = static () => ArrayPool<T>.Shared;
+    private static readonly Lazy<ArrayPool<T>> _pool = new(PoolGenerator);
 
     public readonly Span<T> Span => _array.AsSpan(0, _length);
 
     public static ArrayRental<T> Rent(int length)
-    => new(_pool.Rent(length), length);
+    => new(_pool.Value.Rent(length), length);
     public readonly void Return()
-    => _pool.Return(_array);
+    => _pool.Value.Return(_array);
 }
