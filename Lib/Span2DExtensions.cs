8047f4c356def1c626f86c98f5a33c6a455b1e1f
FaustVX <jodu035@gmail.com>
2023-12-15 22:16:43 +0100

    Added `(RO)Span2D` ext methods: (`Contains`, `IndexOf`, `LastIndexOf`)

--- Lib/Span2DExtensions.cs
+++ Lib/Span2DExtensions.cs
@@ -8,58 +8,50 @@ namespace AdventOfCode;
 #endif
 public static partial class Span2DExtensions
 {
-    public static bool ContainsAnyExcept<T>(this Span2D<T> span, SearchValues<T> values)
-    where T : IEquatable<T>?
-    {
-        for (int y = 0; y < span.Height; y++)
-            if (span.GetRowSpan(y).ContainsAnyExcept(values))
-                return true;
-        return false;
-    }
 
     public static bool ContainsAny<T>(this Span2D<T> span, SearchValues<T> values)
+    where T : IEquatable<T>?
+    => ContainsAny((ReadOnlySpan2D<T>)span, values);
+
+    public static bool ContainsAnyExcept<T>(this Span2D<T> span, SearchValues<T> values)
+    where T : IEquatable<T>?
+    => ContainsAnyExcept((ReadOnlySpan2D<T>)span, values);
+
+    public static bool Contains<T>(this Span2D<T> span, T value)
+    where T : IEquatable<T>?
+    => Contains((ReadOnlySpan2D<T>)span, value);
+
+    public static (int ron, int column) IndexOfAny<T>(this Span2D<T> span, SearchValues<T> values)
+    where T : IEquatable<T>?
+    => IndexOfAny((ReadOnlySpan2D<T>)span, values);
+
+    public static (int ron, int column) IndexOfAnyExcept<T>(this Span2D<T> span, SearchValues<T> values)
+    where T : IEquatable<T>?
+    => IndexOfAnyExcept((ReadOnlySpan2D<T>)span, values);
+
+    public static (int ron, int column) IndexOf<T>(this Span2D<T> span, T value)
+    where T : IEquatable<T>?
+    => IndexOf((ReadOnlySpan2D<T>)span, value);
+
+    public static (int ron, int column) LastIndexOfAny<T>(this Span2D<T> span, SearchValues<T> values)
+    where T : IEquatable<T>?
+    => LastIndexOfAny((ReadOnlySpan2D<T>)span, values);
+
+    public static (int ron, int column) LastIndexOfAnyExcept<T>(this Span2D<T> span, SearchValues<T> values)
+    where T : IEquatable<T>?
+    => LastIndexOfAnyExcept((ReadOnlySpan2D<T>)span, values);
+
+    public static (int ron, int column) LastIndexOf<T>(this Span2D<T> span, T value)
+    where T : IEquatable<T>?
+    => LastIndexOf((ReadOnlySpan2D<T>)span, value);
+
+    public static bool ContainsAny<T>(this ReadOnlySpan2D<T> span, SearchValues<T> values)
     where T : IEquatable<T>?
     {
         for (int y = 0; y < span.Height; y++)
             if (span.GetRowSpan(y).ContainsAny(values))
                 return true;
         return false;
-    }
-
-    public static (int ron, int column) IndexOfAny<T>(this Span2D<T> span, SearchValues<T> values)
-    where T : IEquatable<T>?
-    {
-        for (int y = 0; y < span.Height; y++)
-            if (span.GetRowSpan(y).IndexOfAny(values) is > -1 and var i)
-                return (y, i);
-        return (-1, -1);
-    }
-
-    public static (int ron, int column) IndexOfAnyExcept<T>(this Span2D<T> span, SearchValues<T> values)
-    where T : IEquatable<T>?
-    {
-        for (int y = 0; y < span.Height; y++)
-            if (span.GetRowSpan(y).IndexOfAnyExcept(values) is > -1 and var i)
-                return (y, i);
-        return (-1, -1);
-    }
-
-    public static (int ron, int column) LastIndexOfAny<T>(this Span2D<T> span, SearchValues<T> values)
-    where T : IEquatable<T>?
-    {
-        for (int y = 0; y < span.Height; y++)
-            if (span.GetRowSpan(y).LastIndexOfAny(values) is > -1 and var i)
-                return (y, i);
-        return (-1, -1);
-    }
-
-    public static (int ron, int column) LastIndexOfAnyExcept<T>(this Span2D<T> span, SearchValues<T> values)
-    where T : IEquatable<T>?
-    {
-        for (int y = 0; y < span.Height; y++)
-            if (span.GetRowSpan(y).LastIndexOfAnyExcept(values) is > -1 and var i)
-                return (y, i);
-        return (-1, -1);
     }
 
     public static bool ContainsAnyExcept<T>(this ReadOnlySpan2D<T> span, SearchValues<T> values)
@@ -71,11 +63,11 @@ public static partial class Span2DExtensions
         return false;
     }
 
-    public static bool ContainsAny<T>(this ReadOnlySpan2D<T> span, SearchValues<T> values)
+    public static bool Contains<T>(this ReadOnlySpan2D<T> span, T value)
     where T : IEquatable<T>?
     {
         for (int y = 0; y < span.Height; y++)
-            if (span.GetRowSpan(y).ContainsAny(values))
+            if (span.GetRowSpan(y).Contains(value))
                 return true;
         return false;
     }
@@ -98,6 +90,15 @@ public static partial class Span2DExtensions
         return (-1, -1);
     }
 
+    public static (int ron, int column) IndexOf<T>(this ReadOnlySpan2D<T> span, T value)
+    where T : IEquatable<T>?
+    {
+        for (int y = 0; y < span.Height; y++)
+            if (span.GetRowSpan(y).IndexOf(value) is > -1 and var i)
+                return (y, i);
+        return (-1, -1);
+    }
+
     public static (int ron, int column) LastIndexOfAny<T>(this ReadOnlySpan2D<T> span, SearchValues<T> values)
     where T : IEquatable<T>?
     {
@@ -112,6 +113,15 @@ public static partial class Span2DExtensions
     {
         for (int y = 0; y < span.Height; y++)
             if (span.GetRowSpan(y).LastIndexOfAnyExcept(values) is > -1 and var i)
+                return (y, i);
+        return (-1, -1);
+    }
+
+    public static (int ron, int column) LastIndexOf<T>(this ReadOnlySpan2D<T> span, T value)
+    where T : IEquatable<T>?
+    {
+        for (int y = 0; y < span.Height; y++)
+            if (span.GetRowSpan(y).LastIndexOf(value) is > -1 and var i)
                 return (y, i);
         return (-1, -1);
     }
